2320ab0309042e1b9d57b2c7a3a7e2af34bfb084
99pmore <morenomartin.pablo@gmail.com>
2023-03-09 11:54:03 +0100

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  // { path: 'error-404', component: Error404Component},
+  // { path: '**', pathMatch: 'full', redirectTo: 'error-404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
