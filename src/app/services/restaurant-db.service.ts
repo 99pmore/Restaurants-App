3562af16e3f8c264de41333fffcf40795aa7dd0c
99pmore <morenomartin.pablo@gmail.com>
2023-03-10 13:07:37 +0100

    db services

--- src/app/services/restaurant-db.service.ts
+++ src/app/services/restaurant-db.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class FirestoreService {
+export class RestaurantDBService {
 
   private restaurantCollection!: CollectionReference<DocumentData>
 
